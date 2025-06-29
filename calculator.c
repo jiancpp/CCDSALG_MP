94c0d45e39bb45caea86f10b219a601d4476b88f
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-06-29 19:11:28 +0800

    Initialize branch

--- calculator.c
+++ calculator.c
@@ -4,5 +4,9 @@
 #include "evaluation.h"
 
 int main() {
-    
+    // While loop 
+    // Get Infix
+    // Print Postfix
+    // Print Evaluation
+    // Print New Line
 }
--- conversion.c
+++ conversion.c
@@ -7,8 +7,18 @@
 - Tokenize by operator
 - Push operand to postfix queue
 - Check if operator at the top of the stack is of higher precedence. If no, push to stack. If yes, push all 
-operators in the stack to the postfix queue
+  operators in the stack to the postfix queue
 - Do until infix is done
 
 */
 
+/*
+    getPostfix -> queue
+
+    getOperands -> queue
+
+    getOperators -> stack
+
+    storeOperators -> stack
+
+*/
