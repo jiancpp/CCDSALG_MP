23ed3a7bb67ada7c1a3063864b4bc8f310e15fe5
eepy-elo <shieloheart.lunario@gmail.com>
2025-06-30 14:06:33 +0800

    Updated function prototypes

--- conversion.h
+++ conversion.h
@@ -8,11 +8,17 @@
 typedef struct
 {
     char operator[3];
-    int precedence
+    int precedence;
 } Operator;
 
 Operator    setOperator             (char* operator, int precedence);
 void        initStoredOperators     (Operator storedOperators[18]);
 int         getOperatorIdx          (char* operator, Operator storedOperators[18]);
+bool        isLowerPrecedence       (char* operator1, char* operator2, Operator storedOperators[18]);
+bool        isNumber                (char c);
+bool        isOperand               (char* string);
+void        concatToPostfix         (char* postfix, char* op);
+void        tokenizeInfix           (String256 infix, Queue* infixQueue);
+char*       convertToPostfix        (String256 infix, Queue* postfixQueue);
 
 #endif
