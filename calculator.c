f0eee3db0952152edcc0607d0f0314b82a4a5aeb
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-06-30 21:10:32 +0800

    Reverted changes in calculator

--- calculator.c
+++ calculator.c
@@ -5,11 +5,14 @@
 #include "calculator.h"
 #include "conversion.h"
 #include "evaluation.h"
+#include "queue.h"
 
 int main() {
     String256 infix;
     String256 postfix;
     int answer;
+
+    Queue postfixQueue;
 
     while (scanf("%s", infix) == 1 && strcmp(infix, "QUIT") != 0) {
         postfix[0] = '\0';
@@ -19,7 +22,7 @@ int main() {
         printf("%s\n", infix);
         printf("%s\n", postfix);
 
-        if (evaluatePostfix(postfix))
+        if (evaluatePostfix(postfixQueue, &answer))
             printf("%d\n\n", answer);
         else
             printf("Division by zero error!\n\n");
