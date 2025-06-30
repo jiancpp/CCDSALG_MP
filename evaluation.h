9c6d2c8f2af11337d2ee41c830fc979880cb5137
eepy-elo <shieloheart.lunario@gmail.com>
2025-06-30 14:59:30 +0800

    Added function prototypes

--- evaluation.h
+++ evaluation.h
@@ -5,4 +5,8 @@
 #include "stack.h"
 #include "queue.h"
 
+void        tokenizePostfix      (String256 postfix, Queue* postfixQueue);    
+int         parseToInt           (String256 number);
+int         evaluatePostfix      (Queue postfix);
+
 #endif
