3eace26b6fe64362ae0318a35e30e6c6b5a33d81
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-07-01 08:38:51 +0800

    Added inline comments

--- calculator.c
+++ calculator.c
@@ -5,21 +5,23 @@
 #include "calculator.h"
 #include "conversion.h"
 #include "evaluation.h"
-#include "queue.h"
 
 int main() {
+    // Declare variables
     String256 infix;
     String256 postfix;
     int answer;
 
     while (scanf("%s", infix) == 1 && strcmp(infix, "QUIT") != 0) {
-        postfix[0] = '\0';
+        postfix[0] = '\0'; // Clear postfix string
 
         convertToPostfix(infix, postfix);
 
+        // Display infix and postfix expressions
         printf("%s\n", infix);
         printf("%s\n", postfix);
 
+        // Display answer after evaluation
         if (evaluatePostfix(postfix, &answer))
             printf("%d\n\n", answer);
         else
